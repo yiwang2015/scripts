d16ded2de23d7fa72618bb6d8dc1bb9f743a98f5
bluetib <liu.xiao.hu.net@gmail.com>
2016-07-20 11:51:23 +0800

    added more

---
+++ 11/git_config_user_info.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git config user.name "刘小虎"
+git config user.email "xiaohu.liu@enai.im"
---
+++ 11/polipo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo polipo proxyAddress=0.0.0.0 socksParentProxy="127.0.0.1:1090"  socksProxyType=socks5
